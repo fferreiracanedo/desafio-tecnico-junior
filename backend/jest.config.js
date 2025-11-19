9c20ca3e36e1d93a706caf39df08141f311ea0d1
Mateus Ferreira Canedo <mateusferreiracanedo@gmail.com>
2025-11-19 14:44:13 -0300

    feat: estrutura inicial do backend
    - ConfiguraÃ§Ã£o do Express e Sequelize
    - Arquivos de configuraÃ§Ã£o do banco de dados
    - ConfiguraÃ§Ã£o do Docker
    - Setup de testes com Jest

---
+++ backend/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/migrations/**',
+    '!src/seeders/**'
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true
+};
+
